00b49c9d363e1378f278260300fe081ad2efffc2
liangyapeng <liangyapeng@tydic.com>
2019-03-21 11:19:19 +0800

    众泰易货

---
+++ js/public.js
@@ -0,0 +1,21 @@
+(function (doc, win) {
+    var _root = doc.documentElement,
+        resizeEvent = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        resizeCallback = function () {
+            var clientWidth = _root.clientWidth,
+                fontSize = 20;
+            if (!clientWidth) return;
+            if(clientWidth < 750) {
+                fontSize = 20 * (clientWidth / 375);
+            } else {
+                fontSize = 20 * (750 / 375);
+            }
+            _root.style.fontSize = fontSize + 'px';
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvent, resizeCallback, false);
+    doc.addEventListener('DOMContentLoaded', resizeCallback, false);
+})(document, window);
+
+
+var baseurl='http://192.168.0.164/pec2/';
